817391e13a6dcec8a93c6c066ce9573ab821f0d4
MarkSam999 <marksamoylov2008@gmail.com>
2024-06-27 20:34:35 -0400

    Add footer

--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.db.models import Max
+from django.test import Client, TestCase
+
+from .models import Airport, Flight, Passenger
 
 # Create your tests here.
+class FlightTestCase(TestCase):
+    
+    def setUp(self):
+
+        # Create airports.
+        a1 = Airport.objects.create(code="AAA", city="city A")
+        a2 = Airport.objects.create(code="BBB", city="City B")
+
+        # Create flights.
+        Flight.objects.create(origin=a1, destination=a2, duration=100)
+        Flight.objects.create(origin=a1, destination=a1, duration=200)
+        Flight.objects.create(origin=a1, destination=a2, duration=-100)
