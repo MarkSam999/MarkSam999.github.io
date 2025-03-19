3bd6d024dd4b0028fdf0fbe4ea7604612a73f08e
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-19 09:10:11 -0400

    Auto commit

--- django_practice_1/library_project/library_project/urls.py
+++ django_practice_1/library_project/library_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django import books
+from . import books
 
 urlpatterns = [
     path('', books.urls),
