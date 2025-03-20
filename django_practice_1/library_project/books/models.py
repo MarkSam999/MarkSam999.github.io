ecfe2760adf10559933806b8a32074eacbe84466
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-20 12:57:54 -0400

    Auto commit

--- django_practice_1/library_project/books/models.py
+++ django_practice_1/library_project/books/models.py
@@ -19,4 +19,4 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField(default=timezone.now)
-    isbn = models.CharField(unique=True, default=generate_isbnmax_length=13)
+    isbn = models.CharField(unique=True, default=generate_isbn, max_length=13)
