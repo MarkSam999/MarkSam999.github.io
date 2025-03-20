71427a950e1be7b571f3911b8cc2fe6a7467ceee
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-20 14:08:59 -0400

    Auto commit

--- django_practice_1/library_project/books/data.py
+++ django_practice_1/library_project/books/data.py
@@ -5,5 +5,5 @@ from datetime import
 book = Book(title='Frankenstein', author='Mary Shelley')
 book = Book(title='Prometheus Unbound', author='Percy Bysshe Shelley')
 book = Book(title='Minekampf', author='Adolf Hitler')
-book = Book(title='Refusal of Slavery', author='US Government', published_date=date())
+book = Book(title='Refusal of Slavery', author='US Government', published_date=date(1865,))
 book = Book(title='Molotov-Ribbentrop Pact', author='')
