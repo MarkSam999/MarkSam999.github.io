403e3b38009fc25782cb2a5f1a358279d72296a4
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-20 13:06:42 -0400

    Auto commit

--- django_practice_1/library_project/books/models.py
+++ django_practice_1/library_project/books/models.py
@@ -4,18 +4,6 @@ def random_number():
     body = "".join(str(random.randint(0, 9)) for _ in range(9))
     return body
 
-def generate_isbn():
-    prefix = "978"  # ISBN-13 всегда начинается с 978 или 979
-    body = "".join(str(random.randint(0, 9)) for _ in range(9))
-    partial_isbn = prefix + body
-    check_digit = calculate_isbn13_check_digit(partial_isbn)
-    return partial_isbn + check_digit
-
-def calculate_isbn13_check_digit(isbn_without_check_digit):
-    total = sum((1 if i % 2 == 0 else 3) * int(num) for i, num in enumerate(isbn_without_check_digit))
-    remainder = total % 10
-    return str((10 - remainder) % 10)
-
 from django.db import models
 from django.utils import timezone
 
