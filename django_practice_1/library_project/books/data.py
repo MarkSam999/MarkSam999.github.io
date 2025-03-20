81a50fe31036528f34d2f28844cf6ae523471b91
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-20 14:10:56 -0400

    Auto commit

--- django_practice_1/library_project/books/data.py
+++ django_practice_1/library_project/books/data.py
@@ -2,8 +2,8 @@ from books.models import Book
 from books.models import random_number
 from datetime import date
 
-book_ = Book(title='Frankenstein', author='Mary Shelley')
-book_ = Book(title='Prometheus Unbound', author='Percy Bysshe Shelley')
-book_ = Book(title='Minekampf', author='Adolf Hitler')
+book_1 = Book(title='Frankenstein', author='Mary Shelley')
+book_2 = Book(title='Prometheus Unbound', author='Percy Bysshe Shelley')
+book_3 = Book(title='Minekampf', author='Adolf Hitler')
 book_ = Book(title='Refusal of Slavery', author='US Government', published_date=date(1865,6,18))
 book_ = Book(title='Molotov-Ribbentrop Pact', author='Joseph Stalin and Adolf Hitler')
