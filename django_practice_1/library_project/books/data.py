d1a23e597b066e5029b1a3963fd74194a3823113
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-20 14:07:28 -0400

    Auto commit

--- django_practice_1/library_project/books/data.py
+++ django_practice_1/library_project/books/data.py
@@ -3,5 +3,5 @@ from .models import Book
 book = Book(title='Frankenstein', author='')
 book = Book(title='Prometheus Unbound', author='')
 book = Book(title='Minekampf', author='')
-book = Book(title='', author='')
-book = Book(title='', author='')
+book = Book(title='Refusal of Slavery', author='')
+book = Book(title='Molotov-Ribbentrop Pact', author='')
