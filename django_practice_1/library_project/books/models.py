bcec5bd76a09c31d40fe4e52146b2856af6c676c
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-21 12:16:54 -0400

    Auto commit

--- django_practice_1/library_project/books/models.py
+++ django_practice_1/library_project/books/models.py
@@ -18,4 +18,5 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(max_length=5000)
     available = models.BooleanField(default=False)
-    
+
+    def __str__(self)
