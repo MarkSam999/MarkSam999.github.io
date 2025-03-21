529e076dfd79297508f911ab63c1998b7c65c46b
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-21 11:41:28 -0400

    Auto commit

--- django_practice_1/library_project/books/views.py
+++ django_practice_1/library_project/books/views.py
@@ -3,7 +3,7 @@ from .models import Book, Product
 
 def book_list(request):
     context = {
-        "book_list": Book.objects.all()
+        "book_list": Book.objects.all(),
         "product": Product.objects.all()
     }
     return render(request, 'books/book_list.html', context)
