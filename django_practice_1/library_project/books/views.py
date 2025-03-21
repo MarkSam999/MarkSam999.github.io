f145512c4e7ee37aa8f171e67f12658e8f73697a
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-21 13:21:16 -0400

    Auto commit

--- django_practice_1/library_project/books/views.py
+++ django_practice_1/library_project/books/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Book, Product
 
 def products(request, name):
-    theName = Product.
+    theName = Product.objects.na
     context = {
         "book_list": Book.objects.all(),
         "products": Product.objects.all()
     }
-    return render(request, 'books/products.html', context)
+    return render(request, 'books/products.html', context)
