bfab47a5022f78da88d425fee96d4260e1e45826
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-21 13:21:03 -0400

    Auto commit

--- django_practice_1/library_project/books/views.py
+++ django_practice_1/library_project/books/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book, Product
 
 def products(request, name):
-    
+    theName = 
     context = {
         "book_list": Book.objects.all(),
         "products": Product.objects.all()
