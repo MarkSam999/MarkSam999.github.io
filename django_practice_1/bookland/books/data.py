24ca8bc83c5d0ef5389bece2acb8402eec9cf683
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 10:53:30 -0400

    Auto commit

--- django_practice_1/bookland/books/data.py
+++ django_practice_1/bookland/books/data.py
@@ -2,7 +2,7 @@ from books.models import Book
 
 book_1 = Book(title="A Tale of Two Cities", author="Charles Dickens", year=1859, description="Historical fiction")
 book_1.save()
-book_2 = Book(title="The Little Prince", author="Antoine de Saint-Exupéry", year=1943, description="")
+book_2 = Book(title="The Little Prince", author="Antoine de Saint-Exupéry", year=1943, description="Fantasy, children's fiction")
 book_2.save()
 book_3 = Book(title="The Alchemist", author="Paulo Coelho", year=1988, description="")
 book_3.save()
