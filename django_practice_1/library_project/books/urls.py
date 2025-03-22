5b78a9ba8ce780534a6fa9bfe99ca9cd8d662c91
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 10:31:50 -0400

    Auto commit

--- django_practice_1/library_project/books/urls.py
+++ django_practice_1/library_project/books/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('books/', views.products, name="book_list"),
-    path('products/', views.products, name="book_list"),
+    path('products/<str>', views.products, name="book_list"),
 ]
