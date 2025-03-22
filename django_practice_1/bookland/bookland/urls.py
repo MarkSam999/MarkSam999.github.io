9f7006cdcdb346cf690bffe2fff150971cecdfa9
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 11:07:01 -0400

    Auto commit

--- django_practice_1/bookland/bookland/urls.py
+++ django_practice_1/bookland/bookland/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('books.urls'), name=),
+    path('', include('books.urls'), name='books'),
     path('admin/', admin.site.urls),
 ]
