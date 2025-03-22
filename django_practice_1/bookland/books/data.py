8b9ba93288267488b856ac71f8abbca7e12f8355
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 10:52:18 -0400

    Auto commit

--- django_practice_1/bookland/books/data.py
+++ django_practice_1/bookland/books/data.py
@@ -1,7 +1,6 @@
 from books.models import Book
 
 book_1 = Book(title="A Tale of Two Cities", author="", year=, description="")
-book_1 = Book(title="A Tale of Two Cities", author="", year=, description="")
 book_1.save()
 book_2 = Book(title="The Little Prince", author="", year=, description="")
 book_2.save()
