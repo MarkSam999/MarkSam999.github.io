f538b4f61b6eb99984c1f5e93345efd0c29bcd89
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 11:33:20 -0400

    Auto commit

--- django_practice_1/bookland/books/urls.py
+++ django_practice_1/bookland/books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(', views.books, name="books"),
-    path('/<str:title>', views.details, name="details")
+    path('', views.books, name="books"),
+    path('<str:title>', views.details, name="details")
 ]
