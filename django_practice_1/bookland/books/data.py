ffb638c76ea8be2d2b91350c4cd0425293cf8b8c
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 10:49:38 -0400

    Auto commit

--- django_practice_1/bookland/books/data.py
+++ django_practice_1/bookland/books/data.py
@@ -1,9 +1,9 @@
 from books.models import Book
 
-book_ = Book(title="", )
-book_ = Book(title="", )
-book_ = Book(title="", )
-book_ = Book(title="", )
-book_ = Book(title="", )
+book_ = Book(title="", author="")
+book_ = Book(title="", author="")
+book_ = Book(title="", author="")
+book_ = Book(title="", author="")
+book_ = Book(title="", author="")
 
 
