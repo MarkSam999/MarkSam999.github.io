35ab679e031d3e9637b658d3b3a6af9ff2651a73
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-22 11:04:37 -0400

    Auto commit

--- django_practice_1/bookland/bookland/urls.py
+++ django_practice_1/bookland/bookland/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from bookla
+from bookland import books
 from django.urls import path
 
 urlpatterns = [
