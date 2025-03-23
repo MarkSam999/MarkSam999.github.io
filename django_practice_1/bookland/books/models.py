49f88348724b8debfe23a11b525ff1e39af9377c
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-23 12:16:43 -0400

    Auto commit

--- django_practice_1/bookland/books/models.py
+++ django_practice_1/bookland/books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=20)
-    author = models.ManyToManyField(Author)
+    author = models.ManyToManyField(author)
     year = models.IntegerField()
     description = models.TextField(max_length=5000)
 
