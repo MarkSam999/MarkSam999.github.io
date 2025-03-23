808e5d2936bf518ab85e39573ee0f3364e331927
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-23 12:37:33 -0400

    Auto commit

--- django_practice_1/bookland/books/data.py
+++ django_practice_1/bookland/books/data.py
@@ -2,7 +2,7 @@ from books.models import Book
 from books.models import Author
 from django.utils import timezone
 
-a1 = Author(name="Pushkin", biography="a", birth_date = timezone())
+a1 = Author(name="Pushkin", biography="a", birth_date = timezone(1))
 
 book_1 = Book(title="A Tale of Two Cities", author="Charles Dickens", year=1859, description="Historical fiction")
 book_1.save()
