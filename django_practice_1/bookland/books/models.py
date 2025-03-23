b84ccc52668553cf33719ed6eeb2bf95c3c51c98
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-23 12:12:07 -0400

    Auto commit

--- django_practice_1/bookland/books/models.py
+++ django_practice_1/bookland/books/models.py
@@ -7,4 +7,4 @@ class Book(models.Model):
     description = models.TextField(max_length=5000)
 
     def __str__(self):
-        return f"{self.title} {self.author}"
+        return f"{self.title} by {self.author}"
