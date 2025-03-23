49cff796b2c90afa3598cf585ae58236c3e98083
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-23 12:22:48 -0400

    Auto commit

--- django_practice_1/bookland/books/admin.py
+++ django_practice_1/bookland/books/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book, Author
 
 class BookList(admin.ModelAdmin):
-    list_display = {"title", "author", ""}
+    list_display = {"title", "author", "year"}
 
 admin.site.register(Book)
 admin.site.register(Author)
