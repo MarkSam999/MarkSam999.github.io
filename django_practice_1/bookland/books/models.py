8408ddacf75a31598a2f40cc0dd435a5d6645fbf
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-23 12:19:11 -0400

    Auto commit

--- django_practice_1/bookland/books/models.py
+++ django_practice_1/bookland/books/models.py
@@ -4,11 +4,10 @@ from django.utils import timezone
 class Author(models.Model):
     name = models.CharField(max_length=50)
     biography = models.TextField(max_length=1000)
-    birth_date
+    birth_date = models.DateField()
 
 class Book(models.Model):
     title = models.CharField(max_length=40)
     author = models.ManyToManyField(to=Author)
     year = models.IntegerField()
     description = models.TextField(max_length=5000)
-
