81ba41c7e3927240fb7723d8e0bd0624f1e46889
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 12:23:39 -0400

    Auto commit

--- django_practice_1/bookland/books/admin.py
+++ django_practice_1/bookland/books/admin.py
@@ -7,8 +7,6 @@ class AuthorList(admin.ModelAdmin):
 class BookList(admin.ModelAdmin):
     list_display = ("title", str(Author.objects.all().values().name), "year")
 
-    def author:
-
     def author(self, obj):
         return ", ".join(author.name for author in obj.authors.all())
 
