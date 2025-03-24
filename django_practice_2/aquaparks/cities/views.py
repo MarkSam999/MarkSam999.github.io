b01d0ff53f1ec68a5ebfc56606bbd377cc2504ad
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 17:54:18 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/views.py
+++ django_practice_2/aquaparks/cities/views.py
@@ -3,10 +3,9 @@ from .models import AquaPark, City
 
 def aquaparks(request, id):
     city = City.objects.get(id=id)
-    aquaparks = AquaPark.objects.filter(city=city)
     context = {
         'city': city,
         'cities': City.objects.all(),
-        'aquaparks': aquaparks
+        'aquaparks': AquaPark.objects.all()
     }
     return render(request, "cities/index.html", context)
