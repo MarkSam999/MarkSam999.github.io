e927ef167d5e483f40638c2901c928dfa29d7d4a
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 14:37:38 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/data.py
+++ django_practice_2/aquaparks/cities/data.py
@@ -63,13 +63,13 @@ aquapark_19.save()
 aquapark_20 = AquaPark(name="пляж_2", address="address", description="a")
 aquapark_20.save()
 
-city_ = City
-city_ = City
-city_ = City
-city_ = City
-city_ = City
-city_ = City
-city_ = City
-city_ = City
-city_ = City
-city_ = City
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
+city_ = City.objects.all()[]
