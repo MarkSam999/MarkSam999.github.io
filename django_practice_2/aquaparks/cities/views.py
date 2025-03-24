29e6e463ec4b6d1c81af0607b262bc2fcdbbd20d
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 12:52:04 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/views.py
+++ django_practice_2/aquaparks/cities/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def view(request):
     context = {
         'cities': City.objects.all(),
-        'aquaparks': AquaParks.objects.all()
+        'aquaparks': AquaPark.objects.all()
     }
