f3787fd9e25c1481e53c21b4db4f4f7e6fb8cd27
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 14:01:33 -0400

    Auto commit

--- django_practice_2/aquaparks/aquaparks/urls.py
+++ django_practice_2/aquaparks/aquaparks/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from aquaparks.urls import
 
 urlpatterns = [
     path('', include('aquaparks.urls'), name="aquaparks"),
