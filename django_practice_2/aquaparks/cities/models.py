0e0dd863a2a2b2d219634df21ae8c24fbaad1e75
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 13:01:35 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/models.py
+++ django_practice_2/aquaparks/cities/models.py
@@ -11,7 +11,7 @@ class AquaPark(models.Model):
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=40)
     description = models.TextField(max_length=5000)
-    city = models.ManyToManyField()
+    city = models.ManyToManyField(City)
 
     def __str__(self):
         return self.name
