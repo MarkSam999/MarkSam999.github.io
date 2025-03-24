57b7a45d8fd92fe49da363cf93035df63eab8dd6
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 14:20:03 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/data.py
+++ django_practice_2/aquaparks/cities/data.py
@@ -2,42 +2,42 @@ from cities.models import AquaPark
 from cities.models import Cities
 
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
 aquapark_ = AquaPark(name="", address="", description="a")
-aquapark_
+aquapark_.save()
