7200f9bd5ca0bd4ea9baf9a8a11fecbd252b2cf4
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 13:00:25 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/models.py
+++ django_practice_2/aquaparks/cities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=20)
-    address
+    address = models.CharField(max_length=40)
     description = models.TextField(max_length=5000)
 
     def __str__(self):
