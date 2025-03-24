2cc21b2396b10ca76537deb079cee75040e40b36
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 12:55:32 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/admin.py
+++ django_practice_2/aquaparks/cities/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 class AquaParkList(admin.ModelAdmin):
-    list_display = ("name", "address", )
+    list_display = ("name", "address", "description")
 
 admin.site.register(AquaPark, AquaParkList)
 admin.site.register(City, CityList)
