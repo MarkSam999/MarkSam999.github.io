f4f5d13038a1e28550f164a888a262f2232a9b8d
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 14:54:50 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/views.py
+++ django_practice_2/aquaparks/cities/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import AquaPark, City
 
 def aquaparks(request, id):
-    
     context = {
         'cities': City.objects.all(),
         'aquaparks': AquaPark.objects.all()
