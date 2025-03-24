78753675e8fcedd7c318916b644e240e995db02d
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 12:56:41 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/admin.py
+++ django_practice_2/aquaparks/cities/admin.py
@@ -4,7 +4,7 @@ class AquaParkList(admin.ModelAdmin):
     list_display = ("name", "address", "description", "cities")
 
     def cities(self, obj):
-        return ", ".join([city.name for city])
+        return ", ".join([city.name for city in obj.city.all()])
 
 admin.site.register(AquaPark, AquaParkList)
 admin.site.register(City, CityList)
