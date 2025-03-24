378d0c35a593815f719f413cf98a752227eef55a
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 12:53:17 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/models.py
+++ django_practice_2/aquaparks/cities/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class City(models.Model):
-
+    name = 
 
     def __str__(self):
         return self.name
 
 class AquaPark(models.Model):
-    
+
 
     def __str__(self):
         return self.name
