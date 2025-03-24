f69b80903fc141ce686cd4b946ba2e29937c5443
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 14:19:55 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/data.py
+++ django_practice_2/aquaparks/cities/data.py
@@ -2,41 +2,42 @@ from cities.models import AquaPark
 from cities.models import Cities
 
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
-
+aqua
 aquapark_ = AquaPark(name="", address="", description="a")
+aqua
