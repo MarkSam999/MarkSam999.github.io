3596e4622240c5592ff21984fb02cbf9568f1779
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 12:24:16 -0400

    Auto commit

--- django_practice_1/bookland/books/admin.py
+++ django_practice_1/bookland/books/admin.py
@@ -5,11 +5,11 @@ class AuthorList(admin.ModelAdmin):
     list_display = ("name", "birth_date")
     
 class BookList(admin.ModelAdmin):
-    
-    list_display = ("title", str(author), "year")
-
     def author():
         str(Author.objects.all().values().name)
+    list_display = ("title", str(author), "year")
+
+    
 
 admin.site.register(Book, BookList)
 admin.site.register(Author, AuthorList)
