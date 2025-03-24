5211d51c13b214d6af385713160adc25fd77e9c9
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 14:03:12 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/urls.py
+++ django_practice_2/aquaparks/cities/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('/aquaparks', views.aquaparks, name="")
+    path('/aquaparks', views.aquaparks, name="aquaparks")
 ]
