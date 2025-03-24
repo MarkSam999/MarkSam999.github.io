e9a3556c576f2cd6cf393fd53e185f966189a2bb
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-24 18:14:49 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/views.py
+++ django_practice_2/aquaparks/cities/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from .models import AquaPark, City
 
-def aquaparks(request, id):
-    city = City.objects.get(id=id)
-    aquaparks = AquaPark.objects.filter(city=city)  # Фильтруем аквапарки по городу
-
+def aquaparks(request):
     context = {
-        'city': city,
-        'cities': City.objects.all(),  # Все города для переключения
-        'aquaparks': aquaparks  # Только аквапарки выбранного города
+        'cities': City.objects.all(),
+        'aquaparks': AquaPark.objects.all()
     }
     return render(request, "cities/index.html", context)
