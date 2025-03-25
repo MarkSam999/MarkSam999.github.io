acb82228852901ecb66ed8adcfb6f8d2c8ee0ec0
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-25 10:47:40 -0400

    Auto commit

--- django_practice_3/canvas/canvas/urls.py
+++ django_practice_3/canvas/canvas/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('')),
+    path('', include('courses')),
     path('admin/', admin.site.urls),
 ]
