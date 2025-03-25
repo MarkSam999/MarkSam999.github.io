9c6269fd5ee979789078a458875cf993b063be04
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-25 12:02:42 -0400

    Auto commit

--- django_practice_3/canvas/courses_students/views.py
+++ django_practice_3/canvas/courses_students/views.py
@@ -9,6 +9,6 @@ def courses(request):
 
 def students(request):
     context = {
-        'students': Student.objects.select_related('course').all()
+        'students': Student.objects.all()
     }
     return render(request, "courses_students/students.html", context)
