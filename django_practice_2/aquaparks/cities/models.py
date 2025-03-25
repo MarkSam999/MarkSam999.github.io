0842edcbc212bf4ccd73fd28e5cbf8522cfa9848
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-25 09:42:02 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/models.py
+++ django_practice_2/aquaparks/cities/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class AquaPark(models.Model):
     name = models.CharField(max_length=50)
-    city = models.ForeignKey(City, o)
+    city = models.ForeignKey(City, on_delete=models.)
