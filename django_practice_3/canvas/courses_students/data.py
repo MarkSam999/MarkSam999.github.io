fb5bca751ad1851396ae8df21febaeabad74c46d
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-25 11:04:04 -0400

    Auto commit

--- django_practice_3/canvas/courses_students/data.py
+++ django_practice_3/canvas/courses_students/data.py
@@ -1,10 +1,15 @@
 from courses_students.models import Student, Course
 
 st = Student(name="", email="")
+
 st = Student(name="", email="")
+
 st = Student(name="", email="")
+
 st = Student(name="", email="")
+
 st = Student(name="", email="")
+
 
 
 
