309ec0c51a7877f4de1c4f83bd99a9eefd5391cc
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-25 10:54:04 -0400

    Auto commit

--- django_practice_3/canvas/courses_students/urls.py
+++ django_practice_3/canvas/courses_students/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('courses/', views.courses, name="courses")
+    path('courses/', views.courses, name="courses"),
     path('students')
 ]
