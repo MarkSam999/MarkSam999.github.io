958bcd4f7fafa60f51fbdd72edafc990c9ed4f51
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-25 09:49:08 -0400

    Auto commit

--- django_practice_2/aquaparks/cities/data.py
+++ django_practice_2/aquaparks/cities/data.py
@@ -1,12 +1,12 @@
 from cities.models import City, AquaPark
 
-city_1 = City(name=
+city_1 = City(name="New-York")
 city_1.save()
-city_2 = City(name=
+city_2 = City(name="New-York")
 city_2.save()
-city_1 = City(name=
+city_1 = City(name="New-York")
 city_1.save()
-city_1 = City(name=
+city_1 = City(name="New-York")
 city_1.save()
-city_1 = City(name=
+city_1 = City(name="New-York")
 city_1.save()
