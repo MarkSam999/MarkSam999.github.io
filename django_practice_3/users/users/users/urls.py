c709aeb13b8d7ce590d8018463f95487efb30481
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-27 10:29:17 -0400

    Auto commit

--- django_practice_3/users/users/users/urls.py
+++ django_practice_3/users/users/users/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('')),
+    path('', include('list.urls')),
     path('admin/', admin.site.urls),
 ]
