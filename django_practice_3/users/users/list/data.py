3f27545e19ef01475a7959c56d3aae70b3e10ea6
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-27 10:42:40 -0400

    Auto commit

--- django_practice_3/users/users/list/data.py
+++ django_practice_3/users/users/list/data.py
@@ -11,8 +11,8 @@ user_4.save()
 user_5 = User(username="", email="")
 user_5.save()
 
-user_1 = User.objects
-user_1 = User.objects
-user_1 = User.objects
-user_1 = User.objects
-user_1 = User.objects
+user_1 = User.objects.all()[]
+user_1 = User.objects.all()[]
+user_1 = User.objects.all()[]
+user_1 = User.objects.all()[]
+user_1 = User.objects.all()[]
