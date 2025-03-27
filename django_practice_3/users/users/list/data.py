2c10f75230d725eeae025c605ead716a729d3c7e
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-27 10:37:10 -0400

    Auto commit

--- django_practice_3/users/users/list/data.py
+++ django_practice_3/users/users/list/data.py
@@ -1,12 +1,12 @@
 from list.models import User
 
-user_1 = User(name="", email="")
+user_1 = User(username="", email="")
 user_1.save()
-user_2 = User(name="", email="")
+user_2 = User(username="", email="")
 user_2.save()
-user_3 = User(name="", email="")
+user_3 = User(username="", email="")
 user_3.save()
-user_4 = User(name="", email="")
+user_4 = User(username="", email="")
 user_4.save()
-user_5 = User(name="", email="")
+user_5 = User(username="", email="")
 user_5.save()
