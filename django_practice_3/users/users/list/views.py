81d77f07cdcc9464a152db1c78f6bc7123a3fb22
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-27 11:22:48 -0400

    Auto commit

--- django_practice_3/users/users/list/views.py
+++ django_practice_3/users/users/list/views.py
@@ -3,6 +3,6 @@ from .models import User
 
 def users(request):
     context = {
-        'users': User.objects.all().order_by('firstname')
+        'users': User.objects.all().order_by('username')
     }
     return render(request, "list/index.html", context)
