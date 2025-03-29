412b5c95824e7f24a4e39eb9086b70a8781ee2d3
MarkSam999 <marksamoylov2008@gmail.com>
2025-03-29 14:29:02 -0400

    Auto commit

--- django_practice_4/statics/statics/settings.py
+++ django_practice_4/statics/statics/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '..'),
+    os.path.join(BASE_DIR, '../main_app'),
 )
 
 # Default primary key field type
