3032449a6cf655a46b7d71c17b2853af333a6956
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-04 13:19:27 -0400

    Auto commit

--- nodejs/nodejs_practice_1/app.js
+++ nodejs/nodejs_practice_1/app.js
@@ -1,9 +1,9 @@
 const add = require("./mathOperations.js") ;
 const subtract = require("./mathOperations.js") ;
-const add = require("./mathOperations.js") ;
-const add = require("./mathOperations.js") ;
+const multiply = require("./mathOperations.js") ;
+const divide = require("./mathOperations.js") ;
 
 console.log(add(10,5));
 console.log(subtract(10,5));
 console.log(multiply(10,5));
-console.log(divide(10,5));
+console.log(divide(10,5
