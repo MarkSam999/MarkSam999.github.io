d7138ea59c9cbe616910f55090271a7181748988
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-04 13:16:40 -0400

    Auto commit

--- nodejs/nodejs_practice_1/mathOperations.js
+++ nodejs/nodejs_practice_1/mathOperations.js
@@ -10,7 +10,4 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 }
-console.log(add(10,5));
-console.log(subtract(10,5));
-console.log(multiply(10,5));
-console.log(divide(10,5));
+
