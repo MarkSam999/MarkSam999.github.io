b49421aa8c2c37732877bea143386f177c21270f
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-04 13:38:35 -0400

    Auto commit

--- nodejs/iife.js
+++ nodejs/iife.js
@@ -1,9 +1,9 @@
 (function(){
     const superHero = "batman";
     console.log(superHero);
-});
+})("He");
 
 (function(){
     const superHero = "superman";
     console.log(superHero);
-});
+})("Hey");
