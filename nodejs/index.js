62ce8801318e567296426842106f79c7cc0ae408
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-10 12:34:26 -0400

    Auto commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,1 @@
-const superHero = require("./super-hero")
-
-console.log(superHero.getName());
-superHero.setName("Superman");
-console.log(superHero.getName());
-
-const newSuperHero = require("./super-hero")
-console.log(newSuperHero.getName())
+const SuperHero 
