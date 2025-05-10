2be727b906372e3cf90354b73c7a0803e878bed0
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-10 11:37:56 -0400

    Auto commit

--- django_forms_choice/student/form/views.py
+++ django_forms_choice/student/form/views.py
@@ -14,7 +14,7 @@ def generate_question():
         if num2 > num1:
             num3 = num1 
             num1 = num2
-            num2 = 
+            num2 = num3
     if action == 'multiply':
         correct_answer = num1 * num2
     if action == 'divide':
