074997824b7396ddf5bbc0613335eac3aecd8bc5
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-10 11:40:15 -0400

    Auto commit

--- django_forms_choice/student/form/views.py
+++ django_forms_choice/student/form/views.py
@@ -8,9 +8,12 @@ def generate_question():
     if action == 'add':
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
-        
+
         correct_answer = num1 + num2
     if action == 'subtract':
+        num1 = random.randint(1, 20)
+        num2 = random.randint(1, 10)
+
         correct_answer = num1 - num2
         if num2 > num1:
             num3 = num1 
