b14e0831c6ebec0c6b67f72f2b5cbdaf827dfd8f
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-10 11:11:45 -0400

    Auto commit

--- django_forms_choice/student/form/urls.py
+++ django_forms_choice/student/form/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('math_question', views.generate_question, name='choice.form'),
+    path('math_question', views.math_question_view, name='choice.form'),
     path('success', views.success, name='success')
 ]
