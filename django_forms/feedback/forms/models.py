316afcdc6b5d8974db108f0148b37cb2efe7f751
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-11 08:30:01 -0400

    Auto commit

--- django_forms/feedback/forms/models.py
+++ django_forms/feedback/forms/models.py
@@ -5,4 +5,4 @@ class Review(models.Model):
     movie_title = models.CharField(max_length=200)
     rating = models.IntegerField()
     comment = models.TextField(max_length=5000)
-    
+    created_at = models.DateField(max)
