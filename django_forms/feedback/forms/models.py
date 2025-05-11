9cf42c5c59dbb4baa85e044427e6ca03a3712986
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-11 08:29:16 -0400

    Auto commit

--- django_forms/feedback/forms/models.py
+++ django_forms/feedback/forms/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Review(models.Model):
     name = models.CharField(max_length=100)
     movie_title = models.CharField(max_length=200)
-    rating = models.IntegerField(m
+    rating = models.IntegerField()
