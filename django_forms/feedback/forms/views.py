cdc4396eec9b84d5568834546af1555427ad24e7
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-11 08:25:32 -0400

    Auto commit

--- django_forms/feedback/forms/views.py
+++ django_forms/feedback/forms/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def feedback(request):
     if request.method == "POST":
-        form = FeedbackForm
+        form = FeedbackForm(request.POST)
     return render({'form': form})
