d2f8d15b00a65d411b3f2bdc24743b55d3718c51
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-25 14:10:47 -0400

    Auto commit

--- django/django_register/register/form/views.py
+++ django/django_register/register/form/views.py
@@ -1,11 +1,11 @@
 from from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, red
+from django.shortcuts import render, redirect
 
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect
+            return redirect('home')
         else:
             form = UserCreationForm()
