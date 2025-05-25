55731a5b9d63a6fc815019ee84c3923beedfe6be
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-25 14:11:41 -0400

    Auto commit

--- django/django_register/register/form/views.py
+++ django/django_register/register/form/views.py
@@ -9,4 +9,4 @@ def register(request):
             return redirect('home')
         else:
             form = UserCreationForm()
-    return render(request, "")
+    return render(request, "register.html", {})
