158fe8ae5426e1646c69026d0dd23562c403b78a
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-31 08:11:02 -0400

    Auto commit

--- django/django_register/register_2/form/urls.py
+++ django/django_register/register_2/form/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("home", views.home, name="home"),
-    path("register", views.name="register"),
+    path("register", views.register, name="register"),
     path("login", name="login"),
     path("logout", name="logout")
 ]
