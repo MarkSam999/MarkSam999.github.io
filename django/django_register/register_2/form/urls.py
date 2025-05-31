4ff1cc02dc79b4fc346737852d275751db7e4097
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-31 08:12:30 -0400

    Auto commit

--- django/django_register/register_2/form/urls.py
+++ django/django_register/register_2/form/urls.py
@@ -1,10 +1,10 @@
 from . import views
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, Lo
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("home", views.home, name="home"),
     path("register", views.register, name="register"),
-    path("login", name="login"),
-    path("logout", name="logout")
+    path("login", , name="login"),
+    path("logout", , name="logout")
 ]
