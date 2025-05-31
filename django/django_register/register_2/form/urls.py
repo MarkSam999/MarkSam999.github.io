030bb00661effe4a4247a35f8223f4f943255145
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-31 08:10:12 -0400

    Auto commit

--- django/django_register/register_2/form/urls.py
+++ django/django_register/register_2/form/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("", ),
-    path("", ),
-    path("", ),
-    path("", )
+    path("", name=""),
+    path("", name=""),
+    path("", name=""),
+    path("", name="")
 ]
