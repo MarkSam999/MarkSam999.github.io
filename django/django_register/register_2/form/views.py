b94fecabdb030f974642c8202b004d4b8c45b817
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-31 08:05:03 -0400

    Auto commit

--- django/django_register/register_2/form/views.py
+++ django/django_register/register_2/form/views.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
+    if request.method == ""
     return render(request, "register.html", {"form": form})
