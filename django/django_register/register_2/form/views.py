bc6deab8957a4ad6d81b61667c6ba4ad73a24bd8
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-31 08:04:04 -0400

    Auto commit

--- django/django_register/register_2/form/views.py
+++ django/django_register/register_2/form/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.models import 
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-
+    
