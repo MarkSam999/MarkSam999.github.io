bf0d3efb3418428d9bdc1fefc5ffcb3b0013d9b0
MarkSam999 <marksamoylov2008@gmail.com>
2025-05-31 08:13:14 -0400

    Auto commit

--- django/django_register/register_2/form/urls.py
+++ django/django_register/register_2/form/urls.py
@@ -5,6 +5,5 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path("home", views.home, name="home"),
     path("register", views.register, name="register"),
-    path("login", LoginView.as_view(template_name="login"), name="login"),
-    path("logout", LogoutView.as_view(), name="logout")
-]
+    path("login", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout", LogoutView.as_view(next), name="logout")
