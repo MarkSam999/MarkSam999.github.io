81a9e30340be110b7b29e2813b8e7c6f2ac11131
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-02 08:20:34 -0400

    Auto commit

--- nodejs/module_scope/task/app.js
+++ nodejs/module_scope/task/app.js
@@ -14,7 +14,7 @@ if(user_info.country == "USA"){
 
 if(user_info.income < 2500){
     console.log("Poor.");
-} else if(user_info.income >= 2500) and (user_info.income < 5000){}
+} else if(user_info.income >= 2500) and (user_info.income < 5000){
     console.log("Medium"); 
 } else if(user_info.income >= 5000) and (user_info.income < 10000){
     console.log("More than average"); 
