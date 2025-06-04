f8eb5d2ecebd0a4738046ba448bfa72c885b4399
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-04 09:31:11 -0400

    Auto commit

--- nodejs/module_stuff/super-hero.js
+++ nodejs/module_stuff/super-hero.js
@@ -1,13 +1,13 @@
 class SuperHero {
     constructor (name) {
         this.name = name;
-    }
+    };
 
     getName(){
         return this.name;
-    }
+    };
 
     setName(name){
-        
-    }
+        this.name = name;
+    };
 }
