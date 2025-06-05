23cc159538e9ec0c685e733f373f53d7fe58b61b
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:42:38 -0400

    Auto commit

--- django/django_profile/profil/profil/urls.py
+++ django/django_profile/profil/profil/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.
+from django.contrib.auth.fo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
