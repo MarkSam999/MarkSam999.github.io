bd5a0c4f42d7355ccfd9da529ced8cb418e2d5ac
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:43:23 -0400

    Auto commit

--- django/django_profile/profil/profil/urls.py
+++ django/django_profile/profil/profil/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls")),
-    
+    path(""),
     path("logout/", LogoutView.as_view(next_page='home'))
 ]
