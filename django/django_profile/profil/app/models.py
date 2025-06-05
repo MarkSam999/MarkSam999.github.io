6182995c9697340d126537363905fa27ce50dc4d
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:33:32 -0400

    Auto commit

--- django/django_profile/profil/app/models.py
+++ django/django_profile/profil/app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, )
+    user = models.OneToOneField(User, on)
