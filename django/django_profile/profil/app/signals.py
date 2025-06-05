7be1fcaf00e42af5809081449112d14abb4eac94
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:52:31 -0400

    Auto commit

--- django/django_profile/profil/app/signals.py
+++ django/django_profile/profil/app/signals.py
@@ -9,4 +9,5 @@ def create_user_profile(sender. instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, )
+def save_user_profile(sender, instance, **kwargs):
+    
