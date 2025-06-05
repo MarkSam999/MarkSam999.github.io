15c83115ef8f4f35fd7e836bdaae88610c511808
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:39:42 -0400

    Auto commit

--- django/django_profile/profil/app/views.py
+++ django/django_profile/profil/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
@@ -6,4 +6,4 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return re
+            return redirect
