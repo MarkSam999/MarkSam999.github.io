8b9bceca55b31dc012bc5bd82b307924baa7b494
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:39:02 -0400

    Auto commit

--- django/django_profile/profil/app/views.py
+++ django/django_profile/profil/app/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
-        if form.
+        if form.is_valid():
