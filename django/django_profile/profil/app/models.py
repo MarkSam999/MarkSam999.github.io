6a721bac19abc47c10372968775fceced8e34c9c
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:34:58 -0400

    Auto commit

--- django/django_profile/profil/app/models.py
+++ django/django_profile/profil/app/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.Image
+    avatar = models.ImageField()
+    descrip
