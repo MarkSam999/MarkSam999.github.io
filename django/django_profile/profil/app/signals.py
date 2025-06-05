2322ad57fc514d382350caebbf5501b342f78c18
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-05 10:49:56 -0400

    Auto commit

--- django/django_profile/profil/app/signals.py
+++ django/django_profile/profil/app/signals.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender. instance, created, )
+def create_user_profile(sender. instance, created, **kwargs):
+    if created
