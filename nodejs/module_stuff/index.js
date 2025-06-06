ae15011014c1c459d8342703f483fd6be0d9d00a
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-06 09:32:30 -0400

    Auto commit

--- nodejs/module_stuff/index.js
+++ nodejs/module_stuff/index.js
@@ -5,5 +5,5 @@ console.log(batman.getName());
 batman.setName("Bruce Wayne");
 console.log(batman.getName());
 
-const batman = new SuperHero("Batman");
-console.log(batman.getName());
+const satman = new SuperHero("Satman");
+console.log(Satman.getName())
