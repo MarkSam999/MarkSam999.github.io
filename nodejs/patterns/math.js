f523669a58cba0b2e9a7402a0bb4c76bdc003678
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-06 09:47:18 -0400

    Auto commit

--- nodejs/patterns/math.js
+++ nodejs/patterns/math.js
@@ -1,7 +1,7 @@
 module.exports = (a, b) => {
     let raw = a / b * 100;
-    let result = Math.round(a / b * 100);
-    if(result >= 1){
+    let result = Math.round(raw);
+    if(raw >= 1){
         console.log(result + "%");
     } else {
         console.log("<1%")
