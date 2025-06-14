4959593170ea0603782ce538517970fcf63bb9d7
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-14 08:33:54 -0400

    Auto commit

--- django/django_profile/profil_2/form/models.py
+++ django/django_profile/profil_2/form/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     nickname = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_at = models.Date
+    created_at = models.DateField(auto_now_add=True)
+    
