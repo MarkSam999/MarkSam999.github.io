27f712c4dfa3e06114d1057813d2d5b9513ae659
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-14 08:34:59 -0400

    Auto commit

--- django/django_profile/profil_2/form/models.py
+++ django/django_profile/profil_2/form/models.py
@@ -5,4 +5,4 @@ class Profile(models.Model):
     nickname = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
     bio = models.TextField(max_length=5000)
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=1000)
