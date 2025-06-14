348ab64f2b7c4bac6e6db5cc070185c37b1ac714
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-14 08:41:51 -0400

    Auto commit

--- django/django_profile/profil_2/form/signals.py
+++ django/django_profile/profil_2/form/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from .models import Profile
-from django.contrib.auth.models import 
+from import django.contrib.auth.models.User
 from django.db.models.signals import post_save
 
 @reciever(post_save, sender=User)
