722dc021fc50668053a8f1e861343766788e27f5
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-14 08:06:08 -0400

    Auto commit

--- Math_Crusaders/math_crusaders/practice_games/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class AnswerForm(forms.Form):
-    user_answer = forms.IntegerField(label='Type here...', required=True)
-
--- Math_Crusaders/math_crusaders/practice_games/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('game_practice/', views.play_game, name='game_practice'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
