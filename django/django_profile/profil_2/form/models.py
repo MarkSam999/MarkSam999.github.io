8650fc16c463044ad218491f933af8d9caf58fa9
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-14 08:33:19 -0400

    Auto commit

--- django/django_profile/profil_2/form/models.py
+++ django/django_profile/profil_2/form/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    nickname = models.One
+    nickname = models.OneToOneField(User, on_delete=)
