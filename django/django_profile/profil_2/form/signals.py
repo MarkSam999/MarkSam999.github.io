175fcbc14cef76a3b4cf09654a323942e55c7d5f
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-14 08:40:36 -0400

    Auto commit

--- django/django_profile/profil_2/form/signals.py
+++ django/django_profile/profil_2/form/signals.py
@@ -1,7 +1,7 @@
 from django.dispatch import receiver
 from .models import Profile
 
-@reciever(sender=User)
+@reciever(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
