166eb56d777b752ecaa81d5dedbd61cbfce23dcb
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-16 09:04:49 -0400

    Auto commit

--- nodejs/patterns/index.js
+++ nodejs/patterns/index.js
@@ -1,5 +1,5 @@
 const math = require("./math");
 
 const { add, subtract } = math;
-console.log(math.add(5, 3));
-console.log(math.subtract(5, 3));
+console.log(add(5, 3));
+console.log(subtract(5, 3));
