074a6729a9507dd45921d0f88223e3dbec8d71cf
MarkSam999 <marksamoylov2008@gmail.com>
2025-06-27 10:25:17 -0400

    Auto commit

--- frontend/math quiz/script.js
+++ frontend/math quiz/script.js
@@ -1,17 +1,17 @@
 function oneA(){
-    let answer = 63;
+    var answer = 63;
 }
 
 function oneB(){
-    let answer= 50;
+    var answer= 50;
 }
 
 function oneC(){
-    let answer = 70;
+    var answer = 70;
 }
 
 function oneD(){
-    let answer = 66;
+    var answer = 66;
 }
 
 function check(){
