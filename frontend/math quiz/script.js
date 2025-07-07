d327a996b4067cbe0d387960f4642e605721a6b4
MarkSam999 <marksamoylov2008@gmail.com>
2025-07-07 10:37:27 -0400

    Auto commit

--- frontend/math quiz/script.js
+++ frontend/math quiz/script.js
@@ -5,99 +5,99 @@ let q1n1 = Math.round(Math.random() * 10);
 $("#q1").text(q1n1);
 
 function oneA(){
-    answer1 = 63;
+    a1 = 63;
 }
 
 function oneB(){
-    answer1= 50;
+    a1= 50;
 }
 
 function oneC(){
-    answer1 = 70;
+    a1 = 70;
 }
 
 function oneD(){
-    answer1 = 66;
+    a1 = 66;
 }
 
 function twoA(){
-    answer2 = 12;
+    a2 = 12;
 }
 
 function twoB(){
-    answer2= 15;
+    a2= 15;
 }
 
 function twoC(){
-    answer2 = 14;
+    a2 = 14;
 }
 
 function twoD(){
-    answer2 = 17;
+    a2 = 17;
 }
 
 function threeA(){
-    answer3 = 15;
+    a3 = 15;
 }
 
 function threeB(){
-    answer3= 16;
+    a3= 16;
 }
 
 function threeC(){
-    answer3 = 11;
+    a3 = 11;
 }
 
 function threeD(){
-    answer3 = 12;
+    a3 = 12;
 }
 
 function fourA(){
-    answer4 = 9;
+    a4 = 9;
 }
 
 function fourB(){
-    answer4= 8;
+    a4= 8;
 }
 
 function fourC(){
-    answer4 = 12;
+    a4 = 12;
 }
 
 function fourD(){
-    answer4 = 15;
+    a4 = 15;
 }
 
 function fiveA(){
-    answer5 = 32;
+    a5 = 32;
 }
 
 function fiveB(){
-    answer5= 31;
+    a5= 31;
 }
 
 function fiveC(){
-    answer5 = 30;
+    a5 = 30;
 }
 
 function fiveD(){
-    answer5 = 28;
+    a5 = 28;
 }
 
 function check(){
-    if (answer1 === 63){
+    if (a1 === 63){
         points += 1;
     }
-    if (answer2 === 14){
+    if (a2 === 14){
         points += 1;
     }
-    if (answer3 === 12){
+    if (a3 === 12){
         points += 1;
     }
-    if (answer4 === 8){
+    if (a4 === 8){
         points += 1;
     }
-    if (answer5 === 30){
+    if (a5 === 30){
         points += 1;
     }
     alert("You got " + points + "/5!")
