13b58d25447040d3b8cbbbfd6549f7ed62e6b4eb
MarkSam999 <marksamoylov2008@gmail.com>
2025-07-07 10:54:03 -0400

    Auto commit

--- frontend/math quiz/script.js
+++ frontend/math quiz/script.js
@@ -4,12 +4,12 @@ let actions = ['+', "-", '*', '/']
 
 let q1n1 = Math.round(Math.random() * 10);
 let q1n2 = Math.round(Math.random() * 10);
+if(q1n2 = 0){
+        q1n2 = Math.round(Math.random() * 10);
+    };
 let action = actions[Math.round(Math.random() * 3)];
 if(action == '+'){
     q1ca = q1n1 + q1n2;
-    if(q1n2 = 0){
-        q1n2 = Math.round(Math.random() * 10);
-    };
     q1txt = q1n1 + " " + action + " " + q1n2;
 } else if(action == '-'){
     q1ca = q1n1 - q1n2;
