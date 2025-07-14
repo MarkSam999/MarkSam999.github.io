231b9572551fdc6246927ff61dd51d1bee7f5199
MarkSam999 <marksamoylov2008@gmail.com>
2025-07-14 10:37:07 -0400

    Auto commit

--- frontend/math quiz/script.js
+++ frontend/math quiz/script.js
@@ -39,7 +39,7 @@ $("#q1").text(q1txt);
 
 let choices = [];
 for(let i = 0; i < 4; i++){
-    let choice = Math.round(q1ca + Math.random() * 5 || q1ca - Math.random() * 5);
+    let choice = Math.round(q1ca + Math.random() * 5) || Math.round(q1ca - Math.random() * 5);
     if (choice < 0){
         choice = Math.round(q1ca + Math.random() * 5) || Math.round(q1ca - Math.random() * 5);
     }
