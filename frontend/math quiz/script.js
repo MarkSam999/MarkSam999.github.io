1b98854cced97c3fb31d1504c40898a64083ee42
MarkSam999 <marksamoylov2008@gmail.com>
2025-07-14 10:11:38 -0400

    Auto commit

--- frontend/math quiz/script.js
+++ frontend/math quiz/script.js
@@ -14,7 +14,6 @@ if(action == '+'){
     q1ca = q1n1 - q1n2;
     if (q1n2 > q1n1){
         q1n2 = q1n1;
-        q1n1 = q1n2;
         q1txt = "1. "+ q1n1 + " " + action + " " + q1n2 + " = ?";
     }
     q1txt = "1. "+ q1n1 + " " + action + " " + q1n2 + " = ?";
