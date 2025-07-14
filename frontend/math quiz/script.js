c0f5e25846593d6f56059ead11deba1ed49a2520
MarkSam999 <marksamoylov2008@gmail.com>
2025-07-14 10:16:32 -0400

    Auto commit

--- frontend/math quiz/script.js
+++ frontend/math quiz/script.js
@@ -33,7 +33,7 @@ if(action == '+'){
 $("#q1").text(q1txt);
 
 $("#").onclick(function(){
-    a1 = Math.round(Math.random() * 3);
+    a1 = Math.round(Math.random() * 10 + );
 })
 
 function check(){
