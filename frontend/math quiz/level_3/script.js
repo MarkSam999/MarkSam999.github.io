e1e8662d70c79573e8a1175c4cd12dda75ebd8fe
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-05 10:51:47 -0400

    Auto commit

--- frontend/math quiz/level_3/script.js
+++ frontend/math quiz/level_3/script.js
@@ -21,7 +21,7 @@ for(let i = 0; i < 5; i++){
         if (choice < 0){
             choice = 0;
         }
-        if (choice == choices[i - 1]){
+        if (choice == choices[i - 1] || choice == choices[i - 1] || choice == choices[i - 1]){
             choice = choice + 1 || choice - 1;
         }
         choices.push(choice);
