bfb11fe85dabd104db11c84591bac0d557ca472b
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-05 10:56:34 -0400

    Auto commit

--- frontend/math quiz/level_3/script.js
+++ frontend/math quiz/level_3/script.js
@@ -22,7 +22,7 @@ for(let i = 0; i < 5; i++){
             choice = 0;
         }
         if (choice == choices[i - 1] || choice == choices[i - 2] || choice == choices[i - 3]){
-            choice = choice + Math.round(Math.random() * 5 + 1) || choice - Math.round(Math.random() * 5);
+            choice = choice + Math.round(Math.random() * 5 + 1) || choice - Math.round(Math.random() * 5 + 1);
         }
         choices.push(choice);
     }
