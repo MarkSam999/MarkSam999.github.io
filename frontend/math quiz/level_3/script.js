dfd71aa57575bc8a4e7141d4f969f0e7fc8cbd16
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-08 14:13:09 -0400

    Auto commit

--- frontend/math quiz/level_3/script.js
+++ frontend/math quiz/level_3/script.js
@@ -34,7 +34,7 @@ for(let i = 0; i < 5; i++){
 
     for (let ch = 0; ch < 4; ch++){
         let choiceDiv = document.createElement("div");
-        choiceDiv.innerHTML = "<button>" + (ch + 1) + ")</button>" + "<span>" + choices[ch] + "</span>";
+        choiceDiv.innerHTML = "<button id=''>" + (ch + 1) + ")</button>" + "<span>" + choices[ch] + "</span>";
         $("#questions").append(choiceDiv);
     }
 }
