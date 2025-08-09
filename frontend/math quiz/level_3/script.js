2097ccd4c2c3bb16a8d531044b18e5d1012f9d21
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-09 18:20:55 -0400

    Auto commit

--- frontend/math quiz/level_3/script.js
+++ frontend/math quiz/level_3/script.js
@@ -38,7 +38,7 @@ for(let i = 0; i < max; i++){
 
     for(let div = 0; div < 4; div++){
         let choiceDiv = document.createElement("div");
-        choiceDiv.innerHTML = "<button class='choices' onclick='set(" + i + ", " + choices[div]  + ");'>" + (div + 1) + ")</button>" + "<span  class='numbers'>" + choices[div] + "</span>";
+        choiceDiv.innerHTML = "<button class='choices' onclick='set(" + i + ", " + choices[div]  + ");'>" + (div + 1) + ")</button>" + "<span onclick='set(" + i + ", " + choices[div]  + ");'>" class='numbers'>" + choices[div] + "</span>";
         $("#questions").append(choiceDiv);
     }
     
