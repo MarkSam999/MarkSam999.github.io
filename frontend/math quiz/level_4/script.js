c15b910715bfa9f106204ec5b34bfafb499d4422
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-11 09:12:58 -0400

    Auto commit

--- frontend/math quiz/level_4/script.js
+++ frontend/math quiz/level_4/script.js
@@ -25,7 +25,7 @@ for(let i = 0; i < max; i++){
     } else if(action == '/'){
         n1 = n2 * (1 + Math.floor(Math.random() * 99)) / 10;
         ca = n1 / n2;
-        if (n2 < 2){
+        if (n2 < 0.1){
             n2 += 0.1;
         } else if (n2 > n1){
             res = n2;
