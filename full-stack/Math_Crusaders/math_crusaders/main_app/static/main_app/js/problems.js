ca63b33f3d296647c77fd672cf7f4388cb018daf
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-16 11:12:59 -0400

    Auto commit

--- full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
+++ full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
@@ -55,7 +55,51 @@ function level_2(){
 }
 
 function level_3(){
+    let actions = ['square', 'root']
+let correct_list = [];
+let answer_list = [];
+let max = 5;
+
+for(let i = 0; i < max; i++){
+    let q_num = i + 1;
+    let txt = "";
+    let ca = 0;
+    let n1 = 1 + Math.floor(Math.random() * 9);
+    let action = actions[Math.round(Math.random())];
+    answer_list.push(ca);
+
+    if(action == 'square'){
+        ca = n1 ** 2;
+        txt = q_num + ". "  + n1 + "<sup> 2</sup>" + " = ?";
+    } else if(action == 'root'){
+        ca = 1 + Math.floor(Math.random() * 9);
+        n1 = ca ** 2;
+        txt = q_num + ". "  + " √" + n1 + " = ?";
+    }
     
+    correct_list.push(ca);
+
+    let choices = [];
+
+    let question = document.createElement("h1");
+    question.innerHTML = txt;
+    $("#questions").append(question);
+
+    for(let ch = 0; ch < 4; ch++){
+        let choice = Math.round(ca + 1 + Math.random() * 4) || Math.round(ca - 1 - Math.random() * 4);
+        choices.push(choice);
+    }
+
+    let random = Math.floor(Math.random() * 4);
+    choices[random] = ca;
+
+    for(let div = 0; div < 4; div++){
+        let choiceDiv = document.createElement("div");
+        choiceDiv.innerHTML = "<button class='choices' onclick='set(" + i + ", " + choices[div]  + ");'>" + (div + 1) + ")</button>" + "<span onclick='set(" + i + ", " + choices[div]  + ");' class='numbers'>" + choices[div] + "</span>";
+        $("#questions").append(choiceDiv);
+    }
+    
+};
 }
 
 function level_4(){
