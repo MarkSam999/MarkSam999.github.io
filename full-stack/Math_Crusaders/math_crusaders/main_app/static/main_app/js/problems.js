a3e4a56f538847bdd31f82f17bd3b13095efe135
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-17 11:49:30 -0400

    Auto commit

--- full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
+++ full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
@@ -66,7 +66,7 @@ function level_1(){
             let choiceDiv = document.createElement("div");
             choiceDiv.innerHTML = "<div class='choices' onclick='set(" + i + ", " + choices[div]  + ");'>" + choices[div] + "</div>";
 
-            $("#cell_" + q_num + "_" + (cell + 1)).append(choiceDiv);
+            $("#cell_" + q_num + "_" + (div + 1)).append(choiceDiv);
         }
     }
 
