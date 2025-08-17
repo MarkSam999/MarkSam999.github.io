255ba7c97007765289335aed9617b2321245527f
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-17 11:39:05 -0400

    Auto commit

--- full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
+++ full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
@@ -47,9 +47,9 @@ function level_1(){
         for(let cell = 0; cell < 4; cell++){
             let rowHTML = document.createElement("tr");
             if(cell == 1){
-                rowHTML.id = "row_2";
+                rowHTML.id = "row_1";
             } else if(cell == 3){
-                rowHTML.id = "row_4";
+                rowHTML.id = "row_2";
             }
             
             $("#table_" + q_num).append(rowHTML);
