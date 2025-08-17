bfa6ac85fb0db621a72805a9e863240802ead028
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-17 11:45:13 -0400

    Auto commit

--- full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
+++ full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
@@ -57,7 +57,7 @@ function level_1(){
                 let row = document.createElement("tr");
                 row.id = "row_" + q_num + "_2";
                 $("#table_" + q_num).append(row);
-                $("#")
+                $("#row_" + q_num + "_2";)
             }
 
         }
@@ -272,4 +272,3 @@ function check(){
     }
         
     alert("You got " + points + " out of " + max + "!")
-}
