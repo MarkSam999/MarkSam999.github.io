8d2ab8d4a9e18343e283f93b426b342ac9711d40
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-17 11:13:17 -0400

    Auto commit

--- full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
+++ full-stack/Math_Crusaders/math_crusaders/main_app/static/main_app/js/problems.js
@@ -42,7 +42,7 @@ function level_1(){
 
         for (let div = 0; div < 4; div++){
             let choiceTable = document.createElement("table");
-            choiceTable.inn
+            choiceTable.innerHTML = ""
 
             let choiceDiv = document.createElement("div");
             choiceDiv.innerHTML = "<div class='choices' onclick='set(" + i + ", " + choices[div]  + ");'>" + choices[div] + "</div>";
