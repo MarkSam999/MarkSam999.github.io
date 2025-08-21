5b539f8d7490c29b887cbe474fbcd54a88165828
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-21 15:03:54 -0400

    Auto commit

--- full-stack/NeoMath/neo_math/neo_math/wsgi.py
+++ full-stack/NeoMath/neo_math/neo_math/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'math_crusaders.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neo_math.settings')
 
 application = get_wsgi_application()
