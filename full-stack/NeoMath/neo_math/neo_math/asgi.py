9a710f088d3d75eea064d328606486b62b5ca5de
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-23 13:50:52 -0400

    Auto commit

--- full-stack/NeoMath/neo_math/neo_math/asgi.py
+++ full-stack/NeoMath/neo_math/neo_math/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neo_math.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neomath.settings')
 
 application = get_asgi_application()
