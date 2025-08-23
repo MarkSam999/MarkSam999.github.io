84b6057c163a2005d0843c7b4f9a10f2a8e41308
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-23 14:43:00 -0400

    Auto commit

--- full-stack/NeoMath/neo_math/main_app/static/main_app/js/main_script.js
+++ full-stack/NeoMath/neo_math/main_app/static/main_app/js/main_script.js
@@ -1,14 +1,3 @@
-function handleMouseDown(event) {
-    event.target.style.transform = "translateY(3px)";
-};
-
-function handleMouseUp(event) {
-    setTimeout(() => {
-        event.target.style.transform = "";
-    }, 1);
-};
-
-
 $("#progress").animate({width: "100%"}, 2300);
 setTimeout(() => {
     document.getElementById('loader').style.display = 'none';
