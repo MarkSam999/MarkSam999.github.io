5850e5e4d554e3e38323db6fa5e9de15d246080e
MarkSam999 <marksamoylov2008@gmail.com>
2025-08-30 14:19:22 -0400

    Auto commit

--- full-stack/NeoMath/neo_math/main_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import News_Post
-
-admin.site.register(News_Post)
--- full-stack/NeoMath/neo_math/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Rank, Style
-
-admin.site.register(Profile)
-admin.site.register(Rank)
-admin.site.register(Style)
