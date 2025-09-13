1eb469d159c19bc90a25a8977c6cc717ade6995d
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-13 11:06:08 -0400

    Auto commit

--- giants/NeoMath/java_script/styles.js
+++ giants/NeoMath/java_script/styles.js
@@ -1,6 +1,6 @@
-let styleList = []
+let styleList = [];
 function appendCSS(){
-    let styleList = ['header_signs', 'nav_signs', 'problems', 'progress_bar', 'style', 'text']
+    styleList = ['header_signs', 'nav_signs', 'problems', 'progress_bar', 'style', 'text']
     for(let s = 0; s < 6; s++){
         let styleHTML = document.createElement('link')
         styleHTML.rel = "stylesheet";
