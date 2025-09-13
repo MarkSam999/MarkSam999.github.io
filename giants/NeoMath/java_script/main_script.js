38b8fa7c38a2217e8e2d789df465735852526ab5
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-13 11:32:00 -0400

    Auto commit

--- giants/NeoMath/java_script/main_script.js
+++ giants/NeoMath/java_script/main_script.js
@@ -2,5 +2,5 @@ setTimeout(() => {
     document.getElementById('loader').style.display = 'none';
     document.body.style.visibility = 'visible';
     document.body.style.overflow = 'auto';
-}, 60000);
+}, 3000);
 
