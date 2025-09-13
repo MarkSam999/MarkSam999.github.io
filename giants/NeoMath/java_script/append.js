4bd657b85964b2d9968bf12dcf83d578c5dcac26
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-13 11:18:02 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -10,3 +10,13 @@ function appendCSS(){
         document.head.append(styleHTML);
     };
 };
+function appendJS(){
+    styleList = ['header_signs', 'nav_signs', 'problems', 'progress_bar', 'style', 'text'];
+    for(let s = 0; s < 6; s++){
+        let styleHTML = document.createElement('link')
+        styleHTML.rel = "stylesheet";
+        styleHTML.type = "text/css";
+        styleHTML.href = "../css/" + styleList[s] + ".css";
+        document.head.append(styleHTML);
+    };
+};
