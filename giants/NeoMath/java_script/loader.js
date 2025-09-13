e04061429d679022ad546a733846ee3352df2dd1
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-13 11:40:46 -0400

    Auto commit

--- giants/NeoMath/java_script/loader.js
+++ giants/NeoMath/java_script/loader.js
@@ -3,7 +3,7 @@ document.body.header.style.display = "none";
 setTimeout(() => {
     document.getElementById('loader').style.display = 'none';
     document.body.style.visibility = 'visible';
-    document.body.header.style.display = "inline";
+    document.body.h
     document.body.style.overflow = 'auto';
 }, 3000);
 
