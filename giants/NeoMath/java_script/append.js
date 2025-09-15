625128b054252a1a9caf473705103c75d77ccd22
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-14 20:43:10 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -22,7 +22,7 @@ function appendHTML(){
     progressDiv.append(progressBar);
 
     /* Adds the header with the links */
-    let header = 
+    let header = document.create
 }
 
 /* the function that adds all listed css-files to an html-page */
