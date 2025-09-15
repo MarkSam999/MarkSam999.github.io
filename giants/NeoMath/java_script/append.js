b54d119a16501706a15e2204ea425821581e09ef
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-14 20:30:14 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -9,7 +9,7 @@ function appendHTML(){
 
     let loaderText = document.createElement("p");
     loaderText.innerHTML = "Loading...";
-    loaderText.id = "loader_text"
+    loaderText.id = "loader_text";
     loaderDiv.append(loaderText);
 
     let progressDiv = document.createElement("div");
