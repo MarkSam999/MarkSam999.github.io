db6627cc908e885280e8412a7b6e480e570b9446
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-14 20:41:35 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -3,7 +3,7 @@ let scriptList = [];
 
 /* the function that adds an html-code to any page where this function is called */
 function appendHTML(){
-/* the function that adds an html-code to any page where this function is called */
+    /* Adds loader */
     let loaderDiv = document.createElement("div");
     loaderDiv.id = "loader";
     document.body.append(loaderDiv);
