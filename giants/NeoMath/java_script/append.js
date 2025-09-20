b23851374bb1686d5e8862ccd7b332ab0cf1f0e0
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-20 17:30:12 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -63,7 +63,7 @@ function appendCSS(){
 };
 
 /* the function that adds all listed js-files to an html-page */
-function appendJS(backs){
+function appendJS(relat){
     scriptList = ['loader', 'problems'];
     for(let sc = 0; sc < 2; sc++){
         let scriptHTML = document.createElement('script');
