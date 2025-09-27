1c5d670feed12366df1c43fd133d1c97c6ba84fb
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-27 11:02:13 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -31,7 +31,7 @@ function appendHTML(html_path){
     for(let l = 0; l < 3; l++){
         let link = document.createElement("a");
         link.className = "header_links";
-        link.href = link
+        link.href = linkList[l]
         
         header.append(link);
 
