dc6bde05bc0b8e38c07cd10930e2e13953bc96a9
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-27 11:21:12 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -35,7 +35,7 @@ function appendHTML(html_path){
 
         let linkLogo = document.createElement("div");
         linkLogo.className = "header_signs";
-        linkLogo.id = linkList[l];
+        linkLogo.style.backgroundImage = linkList[l];
 
         header.append(link);
         link.append(linkLogo);
