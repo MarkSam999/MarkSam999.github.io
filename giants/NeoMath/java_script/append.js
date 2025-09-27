75fb7cd4bed911acf02f2df787828f1af77aad72
MarkSam999 <marksamoylov2008@gmail.com>
2025-09-27 12:30:46 -0400

    Auto commit

--- giants/NeoMath/java_script/append.js
+++ giants/NeoMath/java_script/append.js
@@ -4,7 +4,7 @@ let scriptList = [];
 
 /* Adds levels for the course.html page*/
 function appendCourse(){
-    let course = ["Addition", "Subtraction"]
+    let course = ["Addition", "Subtraction", ]
     let lesson = document.createElement("a");
     lesson.className
 }
