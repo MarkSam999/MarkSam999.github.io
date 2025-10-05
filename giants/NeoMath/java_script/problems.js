5bc65628dd8d1ac660458e4e7183fafb2f876909
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-05 09:53:42 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -51,7 +51,6 @@ function lvl1(){
         for(let cell = 0; cell < 4; cell++){
             let cellHTML = document.createElement("td");
             cellHTML.id = "cell_" + q_num + "_" + (cell + 1);
-            cellHTML.className = "choices";
 
             if(cell == 0){
                 let row = document.createElement("tr");
