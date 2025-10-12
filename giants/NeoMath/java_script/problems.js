0fc6c46f2d3ddadd8cfcffd8a161558fd058bf71
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-12 09:37:19 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -85,6 +85,7 @@ function next(){
                     let choiceDiv = document.createElement("div");
                     choiceDiv.id = "div_" + (div + 1);
                     choiceDiv.className = 'choices';
+                    choiceDiv.onclick = 
                     choiceDiv.innerHTML = "<div class='choices' onclick='set(" + choices[div]  + ");'>" + choices[div] + "</div>";
 
                     $("#cell_" + (div + 1)).append(choiceDiv);
