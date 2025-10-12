0c93f3172e08650c2a4cc877278a94e5ca9ed827
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-12 10:13:32 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -59,7 +59,7 @@ function next(){
             for(let r = 0; r < 2; r++){
                 row = document.createElement("tr");
                 choiceTable.append(row);
-                for(let d)
+                for(let d = 0; d < 2; d++)
             }
 
             for (let div = 0; div < 4; div++){
