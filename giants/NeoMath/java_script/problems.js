ff68369faf500880cdc6c703e15e3411250f096e
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-12 09:35:11 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -83,7 +83,7 @@ function next(){
 
             for (let div = 0; div < 4; div++){
                     let choiceDiv = document.createElement("div");
-                    choiceDiv.id = "div_"
+                    choiceDiv.id = "div_" + div
                     choiceDiv.innerHTML = "<div class='choices' onclick='set(" + choices[div]  + ");'>" + choices[div] + "</div>";
 
                     $("#cell_" + (div + 1)).append(choiceDiv);
