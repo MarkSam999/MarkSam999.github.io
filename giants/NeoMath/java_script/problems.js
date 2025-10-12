c10db6bf36830a39e00e8e79fa5d69d71d3ecb8b
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-12 10:57:22 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -18,7 +18,7 @@ function start(level){
     q_num += 1;
     let n1 = 1 + Math.round(Math.random() * 9);
     let n2 = 1 + Math.round(Math.random() * 9);
-    let action = actions[Math.round(Math.random() * actions)];
+    let action = actions[Math.round(Math.random() * actions.length / 2)];
 
 
 }
