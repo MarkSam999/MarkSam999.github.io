8185d322a14c896d298362295391ac572479fdd6
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-12 11:05:21 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -52,31 +52,7 @@ function next(){
         
 
         
-            let question = document.createElement("h1");
-            question.id = "currQues";
-            question.innerHTML = q_num + ". " + n1 + " " + action + " " + n2 + " = ?";
-            $("#questions").append(question);
-
-            let choiceTable = document.createElement("table");
-            choiceTable.id = "table_" + q_num;
-            $("#questions").append(choiceTable);
             
-            let d_num = 0;
-            for(let r = 0; r < 2; r++){
-                row = document.createElement("tr");
-                choiceTable.append(row);
-                for(let d = 0; d < 2; d++){
-                    d_num += 1;
-                    cell = document.createElement("td");
-                    cell.id = "cell_" + d_num;
-                    cell.className = 'choices';
-                    cell.innerHTML = choices[d_num - 1];
-                    console.log(cell.innerHTML);
-                    cell.onclick = function(){set(choices[d_num - 1])};
-
-                    row.append(cell);
-                }
-            }
             $("#currQues").text(q_num + ". " + n1 + " " + action + " " + n2 + " = ?");
 
             for(let cell = 0; cell < 4; cell++){
