4a7dee9f9a7186a75f9e8585c9a30bd21a495045
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-14 18:20:12 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -48,8 +48,8 @@ function next(){
     $("#currQues").text(q_num + ". " + n1 + " " + action + " " + n2 + " = ?");
 
     for(let cell = 0; cell < 4; cell++){
-        $("#div_" + cell + 1)).text(choices[cell]);
-        $("#div_" + (cell + 1)).onclick = function(){set(choices[cell])};
+        $("#div_" + cell).text(choices[cell]);
+        $("#div_" + cell).onclick = function(){set(choices[cell])};
     }
 }
 
