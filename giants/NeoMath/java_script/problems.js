c9c415ef9a801162c4ff77f817445890b905e07b
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-21 11:00:52 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -100,7 +100,7 @@ function gen(){
             n2 = n1;
             n1 = res;
         }
-    } else if()
+    } else if(action == "")
 
     for(let ch = 0; ch < 4; ch++){
         let choice = Math.round(ca + 1 + Math.random() * 4) || Math.round(ca - 1 - Math.random() * 4);
