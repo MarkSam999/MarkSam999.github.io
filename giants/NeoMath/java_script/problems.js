5286dd7a0800e6027f78afbb821670e996f1d447
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-21 17:37:07 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -83,7 +83,6 @@ function gen(){
             n1 = nx;
             ca = n1 - n2;
         }
-        txt = q_num + ". " + n1 + " " + action + " " + n2 + " = ?";
     } else if(action == "*"){
         ca = n1 * n2;
     } else if(action == "/"){
