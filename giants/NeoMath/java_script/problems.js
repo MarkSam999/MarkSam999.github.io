01fde3d7451bdfb2149e31f146a4c1b46db219d2
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-21 11:10:48 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -318,12 +318,11 @@ let actions = [];
 let action = 0;
 let ca = 0;
 let choices = [0, 0, 0, 0];
-let yourAnswer = 0;
 let max = 5;
 let points = 0;
 let q_num = 0;
 let n1 = 0;
 let perc = 0;
 let n2 = 0;
-
+let yourAnswer = 0;
 
