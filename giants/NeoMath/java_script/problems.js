19abfa3a13134aa7b376ff89872c123fc67c44ca
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-21 18:08:47 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -142,40 +142,6 @@ function replay(){
     next();
 }
 
-function lvl5(){
-
-    for(let i = 0; i < max; i++){
-        let q_num = i + 1;
-        let ca = 0;
-        let n1 = 1 + Math.round(Math.random() * 49);
-        let perc = 5 * (1 + Math.round(Math.random() * 19));
-
-        ca = Math.round(n1 / 10 * perc) / 10;
-
-        correct_list.push(ca);
-
-        let choices = [];
-
-        let question = document.createElement("h1");
-        question.innerHTML = q_num + ". " + n1 + " → " + perc + "% = ?";
-        $("#questions").append(question);
-
-        for(let ch = 0; ch < 4; ch++){
-            let choice = Math.round(ca + 1 + Math.random() * 69) / 10 || Math.round(ca - 1 - Math.random() * 69) / 10;
-            choices.push(choice);
-        }
-
-        let random = Math.floor(Math.random() * 4);
-        choices[random] = ca;
-
-        for (let div = 0; div < 4; div++){
-            let choiceDiv = document.createElement("div");
-            choiceDiv.innerHTML = "<button class='choices' onclick='set(" + i + ", " + choices[div]  + ");'>" + (div + 1) + ")</button>" + "<span onclick='set(" + i + ", " + choices[div]  + ");' class='numbers'>" + choices[div] + "</span>";
-            $("#questions").append(choiceDiv);
-        }
-    }
-}
-
 // Global values in alphabetical order
 let actions = [];
 let action = 0;
