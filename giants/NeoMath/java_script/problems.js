702cb98578c5e21a2e0bcdd1b6b85e6d377ad48f
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-22 14:35:55 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -153,6 +153,7 @@ function replay(){
 function timer(){
     let time = setInterval(function(){
         seconds -= 1;
+        
         console.log(seconds);
         if(seconds == 0){
             clearInterval(time);
