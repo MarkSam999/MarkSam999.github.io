2f3f5d8a0212ab201c5d86a9f92302f6d45776d3
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-23 18:30:07 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -161,7 +161,7 @@ function timer(){
             setTimeout(() => {
                 for(let ch = 0; ch < 4; ch++){
                     if($("div_" + ch).text() == ca){
-                        $("div_" + ch).css("text-shadow", "0 0 5px green, 0 0 15px green, 0 0 25px green, 0 0 35px green, 0 0 45px green,");
+                        $("div_" + ch).css("text-shadow", "0 0 45px green");
                     } else {
                         $("div_" + ch).css("text-shadow", "0 0 45px red");
                     }
