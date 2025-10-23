6ce9c7cf100db993a83eff53ad05a99b72fd1bb9
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-23 17:47:25 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -85,6 +85,7 @@ function set(value){
 function gen(){
     q_num += 1;
     seconds = 30;
+    
     console.log(seconds);
     n1 = 1 + Math.round(Math.random() * 9);
     n2 = 1 + Math.round(Math.random() * 9);
