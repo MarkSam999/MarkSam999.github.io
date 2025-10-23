b6b53e57a2034c67d2c0f7c75338c8f744f5a330
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-23 18:22:52 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -158,7 +158,7 @@ function timer(){
         if(seconds == 0){
             clearInterval(time);
             for(let ch = 0; ch < 4; ch++){
-                if($("div_" + ch).text())
+                if($("div_" + ch).text() == )
             }
             setTimeout(() => {
                 alert("The time is over!");
