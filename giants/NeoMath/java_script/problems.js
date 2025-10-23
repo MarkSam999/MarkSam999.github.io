d97b407155e1f7c2facbbbfd681b5c2d7eac6e38
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-23 18:34:35 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -169,10 +169,10 @@ function show(){
     for(let ch = 0; ch < 4; ch++){
         if($("#div_" + ch).text() == ca){
             $("#div_" + ch).css("text-shadow", "0 0 5px green, 0 0 15px green, 0 0 25px green, 0 0 35px green, 0 0 45px green");
-                } else {
-                    $("#div_" + ch).css("text-shadow", "0 0 5px red, 0 0 15px red, 0 0 25px red, 0 0 35px red, 0 0 45px red");
-                }
-            };
+        } else {
+            $("#div_" + ch).css("text-shadow", "0 0 5px red, 0 0 15px red, 0 0 25px red, 0 0 35px red, 0 0 45px red");
+        }
+    };
 }
 
 // Global values in alphabetical order
