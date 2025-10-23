a6f0e99b3017c1bab5d76865c7a47e7442198556
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-23 18:52:36 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -171,7 +171,7 @@ function timer(){
 // Shows which choice is correct by changing the lightning color
 function show(){
     $(".choices").click(function(){
-        console.log("Error")
+        console.log("")
     });
     $(".choices").css("cursor", "not-allowed");
 
