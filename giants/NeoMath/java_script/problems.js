519cd487480e5130167976f1701af93ee8f6bfcf
MarkSam999 <marksamoylov2008@gmail.com>
2025-10-23 18:53:11 -0400

    Auto commit

--- giants/NeoMath/java_script/problems.js
+++ giants/NeoMath/java_script/problems.js
@@ -171,7 +171,7 @@ function timer(){
 // Shows which choice is correct by changing the lightning color
 function show(){
     $(".choices").click(function(){
-        console.log("Error 228: the time is up or ")
+        console.log("Error 228: the time is up or the answer is already chosen!")
     });
     $(".choices").css("cursor", "not-allowed");
 
